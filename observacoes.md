fce309f6a514f92df9a75abe9687085600ee642d
Reinildo Reis <reinildo@gmail.com>
2025-09-13 21:57:55 +0000

    work on api/status

---
+++ observacoes.md
@@ -0,0 +1,1 @@
+parei aula dia 20 time: 19:00
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,47 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "alunos do curso são acima da média" });
+  const updatetAt = new Date().toISOString();
+
+  const maxConnectionsQueryResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = maxConnectionsQueryResult.rows[0].max_connections;
+
+  const postgresVersionQueryResult = await database.query(
+    "SHOW server_version;",
+  );
+  const version = postgresVersionQueryResult.rows[0].server_version;
+
+  const dbName = process.env.POSTGRES_DB;
+  const openedConnectionsQueryResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const openedConnections = openedConnectionsQueryResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatetAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+        version: version,
+      },
+    },
+  });
 }
 
 export default status;
+
+/*
+{
+  updated_at: '2022-12-06T03:30:55.000Z',
+  dependencies: {
+    database: {
+      max_connections: 100,
+      opened_connections: 9
+    }
+  }
+}
+  */
