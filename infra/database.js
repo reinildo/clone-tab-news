dd1feb1994bd09bac41e396247e4cb842dd29d5a
Reinildo <reinildo@nucleoti.com>
2025-10-20 15:41:08 -0300

    add logs env

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,14 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("////////////");
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
